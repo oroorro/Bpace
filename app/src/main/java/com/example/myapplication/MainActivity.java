522baecbd49bc79a07f325d674d64f2b762fdd27
Oro Kang <orogang00@gmail.com>
2024-02-04 20:43:03 -0500

    Implemented adding editText dynamically, started on indenting

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,68 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.UitestSectionBinding;
 
 public class MainActivity extends AppCompatActivity {
     //what is this exactly ? is it refering itself?
     private ActivityMainBinding binding;
+    private UitestSectionBinding bindingToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //Testing UI
+//        bindingToolBar = UitestSectionBinding.inflate(getLayoutInflater());
+//        setContentView(bindingToolBar.getRoot());
 
         binding.openButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, NoteActivity.class);
                 startActivity(intent);
+                //make();
             }
-        });
 
+        }
+        );
+
+
+    }
+
+    public void make(){
+        EditText newEditText = new EditText(this);
+        // Optionally, set some properties on the EditText
+        newEditText.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                100
+        ));
+        newEditText.setHint("hello");
+        newEditText.setPadding(0,0,0,40);
+        newEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+        newEditText.setId(View.generateViewId());
+        //newEditText.setMaxLines(1);
+        newEditText.setBackgroundColor(Color.parseColor("#FFC0CB"));
+        //make a new editText
+
+
+        ConstraintLayout layout = findViewById(R.id.root);
+        layout.addView(newEditText);
+        //binding.linearMain.addView(newEditText);
+        //binding.linearContent.addView(newEditText);
     }
 }
--- app/src/main/java/com/example/myapplication/NoteActivity.java
+++ app/src/main/java/com/example/myapplication/NoteActivity.java
@@ -2,21 +2,13 @@ package com.example.myapplication;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
-import android.text.Spannable;
-import android.text.TextWatcher;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import com.example.myapplication.databinding.NoteMainBinding;
 
 public class NoteActivity extends AppCompatActivity {
@@ -25,7 +17,26 @@ public class NoteActivity extends AppCompatActivity {
 
     private EditText edittextView;
 
-    private Button colorButton;
+    //saves reference to currently focused editText
+    private EditText currentEdittext;
+
+
+    private String[] colorList = {
+            "#FF0000",
+            "#00FF00",
+            "#0000FF",
+            "#FFFF00",
+            "#FF00FF",
+            "#00FFFF",
+            "#FFA500",
+            "#FFFFFF",
+    };
+
+    //used to rotate colors for colorList
+    private int colorIndex = 0;
+
+    //identifier for editText, gets incremented on creation
+    private int editTextCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,73 +46,104 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         edittextView = binding.plainTextInput;
-//        colorButton = findViewById(R.id.colorButton);
+        edittextView.setMaxWidth(10);
 
-        edittextView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-        edittextView.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_edittext_background));
+        //edittextView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        //edittextView.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.custom_edittext_background));
 
 
+        /**
+         * new line triggered key listner to edittextView
+         */
         edittextView.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
                 if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                    //do something.
-
-                    binding.buttonIndent.setText("new");
+                    binding.buttonIndent.setText("Indent+");
+                    makeNewEditText(edittextView.getId(), edittextView);
                     return true;
                 }
                 return false;
             }
         });
 
+        //indent button will cause the currently focused editText to become a part of the editText above it
+        binding.buttonIndent.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
-//        edittextView.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//                // Not used in this example.
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                // Check if the change includes a newline character.
-//                if (s.toString().indexOf("\n") != -1) {
-//                    // change indent button when a newline is detected.
-//
-//                    binding.buttonIndent.setText("new");
-//
-//                    onNewLineDetected();
-//                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                // Not used in this example.
-//            }
-//
-//            // Define the specific function to be called when a newline is detected.
-//            private void onNewLineDetected() {
-//                // Your function's code here.
-//                // For example, display a Toast message.
-//                Log.i("ENTER","enter pressed");
-//            }
-//        });
+            }
+
+        });
 
 
-//        colorButton.setOnClickListener(new Button.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                buttonEditText(view);
-//            }
-//        });
+
+
     }
 
     /**
-     * sets new color to editText value starting from 5 to 10 indices
-     * @param view
+     * creates new editText view, this method gets triggered
+     * when user gives new line character inside of editText that they
+     * were writing.
+     * @param editTextId editText's id
+     * @param triggeredEditText editText which has received a new line character
      */
-//    public void buttonEditText(View view){
-//        BackgroundColorSpan backgroundColorSpan = new BackgroundColorSpan(Color.YELLOW);
-//        edittextView.getText().setSpan(backgroundColorSpan, 5, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//    }
+    public void makeNewEditText(int editTextId, EditText triggeredEditText){
+        EditText newEditText = new EditText(this);
+
+        //properties for editText
+        newEditText.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                100
+        ));
+        newEditText.setHint("hello");
+        newEditText.setPadding(0,0,0,40);
+        newEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        //setting maxWidth for new editText same as parent;linearContentLayout
+        int parentWidth = binding.linearContent.getWidth();
+        Log.i("parentWidth",""+parentWidth);
+        newEditText.setMaxWidth(parentWidth);
+
+        //created editTextId for newEditText
+
+        //int newEditTextId = View.generateViewId();
+        newEditText.setId(editTextCount);
+        newEditText.setMaxLines(1);
+        newEditText.setBackgroundColor(Color.parseColor(colorList[colorIndex % 8]));
+        colorIndex++;
+
+        //adds eventListner on newEditText to create further editTexts
+        newEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    binding.buttonIndent.setText("new");
+                    makeNewEditText(editTextCount, newEditText);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //adds focusListener to newly created editText
+        newEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                //when focused make that editText to be currentEdittext
+                if (hasFocus) {
+                    currentEdittext = newEditText;
+                }
+            }
+        });
+
+
+        editTextCount++;
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearContent);
+        //getting index of triggeredEditText
+        int index = layout.indexOfChild(triggeredEditText);
+        //placing newly created editText right next to triggeredEditText
+        layout.addView(newEditText,index + 1);
+    }
 
 }
